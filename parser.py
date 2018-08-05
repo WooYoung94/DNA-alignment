12d5e4cd912bec3a13383292a931df907fbe71e3
Lactobacillus <cyw314@gmail.com>
2018-08-05 22:11:39 +0900

    first

---
+++ parser.py
@@ -0,0 +1,31 @@
+import os
+import sys
+
+if __name__ == '__main__':
+
+	seq = dict()
+
+	with open(sys.argv[1], 'r') as fs:
+
+		raw = fs.readlines()
+
+	for line in raw:
+
+		if 'chr' in line:
+
+			name = line.replace('\n', '')
+			seq[name] = list()
+
+		else:
+
+			seq[name].append(line.replace('\n', ''))
+
+	for key, val in seq.items():
+
+		seq[key] = ''.join(val)
+
+	for key, val in seq.items():
+
+		with open(os.path.join(os.getcwd(), key + '.fa'), 'w') as fs:
+
+			fs.write(val)
