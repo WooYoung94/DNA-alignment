cb57234a401cee1de5156cc7a4eebdc9a2bdd8c7
Youngwoo Cho <cyw314@gmail.com>
2018-08-07 20:24:58 +0900

    gg

--- model/model.py
+++ model/model.py
@@ -54,25 +54,17 @@ class seqMLP(nn.Module):
 
 		out1 = seq1
 		out1 = self.s1fc1(out1)
-		out1 = F.relu(out1, inplace = True)
 		out1 = self.s1fc2(out1)
-		out1 = F.relu(out1, inplace = True)
 		out1 = self.s1fc3(out1)
-		out1 = F.relu(out1, inplace = True)
 
 		out2 = seq2
 		out2 = self.s1fc1(out2)
-		out2 = F.relu(out2, inplace = True)
 		out2 = self.s1fc2(out2)
-		out2 = F.relu(out2, inplace = True)
-		out2 = self.s1fc3(out2)
-		out2 = F.relu(out2, inplace = True)	
+		out2 = self.s1fc3(out2)	
 
 		out = torch.cat((out1, out2, gap), dim = 1)
 		out = self.fc4(out)
-		out = F.relu(out, inplace = True)	
 		out = self.fc5(out)
-		out = F.relu(out, inplace = True)	
 		out = self.fc6(out)
 
 		return out
