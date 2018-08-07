60cb71c8e82fdbf460f1d9f025bca29fda748df9
Youngwoo Cho <cyw314@gmail.com>
2018-08-07 16:43:25 +0900

    again

--- model/model.py
+++ model/model.py
@@ -8,24 +8,39 @@ class seqMLP(nn.Module):
 
 		super(seqMLP, self).__init__()
 
-		self.fc1 = nn.Linear(257, 1024)
-		self.fc2 = nn.Linear(1024, 1024)
-		self.fc3 = nn.Linear(1024, 1024)
-		self.fc4 = nn.Linear(1024, 1024)
-		self.fc5 = nn.Linear(1024, 1024)
-		self.fc6 = nn.Linear(1024, 2)
+		self.s1fc1 = nn.Linear(257, 512)
+		self.s1fc2 = nn.Linear(512, 512)
+		self.s1fc3 = nn.Linear(512, 256)
+		
+		self.s2fc1 = nn.Linear(257, 512)
+		self.s2fc2 = nn.Linear(512, 512)
+		self.s2fc3 = nn.Linear(512, 256)
+
+		self.fc4 = nn.Linear(513, 256)
+		self.fc5 = nn.Linear(256, 256)
+		self.fc6 = nn.Linear(256, 2)
 
 	def forward(self, s1, s2, gap):
 
 		gap = gap.reshape(-1, 1)
 
-		out = torch.cat((s1, s2, gap), dim = 1)
-		out = self.fc1(out)
-		out = F.relu(out, inplace = True)
-		out = self.fc2(out)
-		out = F.relu(out, inplace = True)	
-		out = self.fc3(out)
-		out = F.relu(out, inplace = True)	
+		out1 = s1
+		out1 = self.s1fc1(out1)
+		out1 = F.relu(out1, inplace = True)
+		out1 = self.s1fc2(out1)
+		out1 = F.relu(out1, inplace = True)
+		out1 = self.s1fc3(out1)
+		out1 = F.relu(out1, inplace = True)
+
+		out2 = s2
+		out2 = self.s1fc1(out2)
+		out2 = F.relu(out2, inplace = True)
+		out2 = self.s1fc2(out2)
+		out2 = F.relu(out2, inplace = True)
+		out2 = self.s1fc3(out2)
+		out2 = F.relu(out2, inplace = True)	
+
+		out = torch.cat((out1, out2, gap), dim = 1)
 		out = self.fc4(out)
 		out = F.relu(out, inplace = True)	
 		out = self.fc5(out)
