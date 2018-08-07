ff9a8971d6d561b02758c6fd49b5634583aa5145
Youngwoo Cho <cyw314@gmail.com>
2018-08-07 20:45:05 +0900

    gg

--- model/util.py
+++ model/util.py
@@ -5,5 +5,5 @@ def get_onehot(labels, dim=5):
 	batch_size = labels.size(0)
 	channel_size = labels.size(1)
 	out = torch.zeros(batch_size, channel_size, dim)
-	out[np.arange(batch_size), np.arrange(channel_size), labels.long()] = 1
+	out[np.arange(batch_size), np.arange(channel_size), labels.long()] = 1
 	return out
