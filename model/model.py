3ef67b6ab589d8eefeefec8c48732704f67e0c6e
Youngwoo Cho <cyw314@gmail.com>
2018-08-07 20:16:23 +0900

    gg

--- model/model.py
+++ model/model.py
@@ -2,23 +2,51 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+class LinearBlock(nn.Module):
+
+	def __init__(self, input_dim, output_dim, norm='none', activation='relu'):
+		super(LinearBlock, self).__init__()
+		layers = []
+
+		if norm == 'bn':
+			self.norm = nn.BatchNorm1d(output_dim)
+		elif norm=='none':
+			self.norm = None
+
+		if activation == 'relu':
+			self.activation = nn.ReLU(inplace=True)
+		elif norm=='none':
+			self.activation = None
+
+		layers += [nn.Linear(input_dim, output_dim)]
+		self.main = nn.Sequential(*layers)
+
+	def forward(self, x):
+		x = self.main(x)
+		if self.norm:
+			x = self.norm(x)
+		if self.activation:
+			x = self.activation(x)
+		return x
+
+
 class seqMLP(nn.Module):
 
 	def __init__(self):
 
 		super(seqMLP, self).__init__()
 
-		self.s1fc1 = nn.Linear(128, 256)
-		self.s1fc2 = nn.Linear(256, 256)
-		self.s1fc3 = nn.Linear(256, 256)
+		self.s1fc1 = LinearBlock(128, 256, norm='bn')
+		self.s1fc2 = LinearBlock(256, 256, norm='bn')
+		self.s1fc3 = LinearBlock(256, 256, norm='bn')
 		
-		self.s2fc1 = nn.Linear(128, 256)
-		self.s2fc2 = nn.Linear(256, 256)
-		self.s2fc3 = nn.Linear(256, 256)
+		self.s2fc1 = LinearBlock(128, 256, norm='bn')
+		self.s2fc2 = LinearBlock(256, 256, norm='bn')
+		self.s2fc3 = LinearBlock(256, 256, norm='bn')
 
-		self.fc4 = nn.Linear(513, 256)
-		self.fc5 = nn.Linear(256, 256)
-		self.fc6 = nn.Linear(256, 2)
+		self.fc4 = LinearBlock(513, 256, norm='bn')
+		self.fc5 = LinearBlock(256, 256, norm='bn')
+		self.fc6 = LinearBlock(256, 2, norm='bn')
 
 	def forward(self, seq1, seq2, gap):
 
