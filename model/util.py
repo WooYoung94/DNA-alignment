f69ecd60fa5414d5e5d0bf0a1dd016576f9f14ed
Youngwoo Cho <cyw314@gmail.com>
2018-08-20 18:24:20 +0900

    gg

--- model/util.py
+++ model/util.py
@@ -29,4 +29,19 @@ if __name__ == '__main__':
 
 		log.sort()
 
+	elif sys.argv[1] == 'delta':
+
+		with open(sys.argv[2], 'r') as fs:
+
+			text = fs.read().split('=')[1].split('\n')[:-1]
+
+		for t in text:
+
+			l1 = int(t.split(',')[0].split(':')[-1].strip().split('/')[0][2:-1])
+			l2 = int(t.split(',')[0].split(':')[-1].strip().split('/')[1][1:-2])
+			log.append((l1, l2))
+
+		log.sort()
+		log = [l[0]-l[1] for l in log]
+
 	print(log)
